11b73df5f92d1b34a71ff0f48019126d41c90408
Andrew Roska <andrew.l.roska@gmail.com>
2019-01-08 12:24:08 -0800

    wrote basic js

---
+++ app.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+/* eslint-disable strict */
+var kids = prompt('How many kids do you have?');
+console.log('number of kids:', kids);
+
+var guitars = prompt('How many guitars do you have?');
+console.log('number of guitars:', guitars);
+
+if (kids === '3' || kids === '4'){
+    alert('yeah, you are correct.  too many kids');
+}
+
+if (parseInt(kids) === 3 && parseInt(guitars) === 3){
+    alert('yeah, you are right');
+} else {
+    alert('you are wrong');
+}
